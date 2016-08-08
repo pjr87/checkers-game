fddff334d820a8cf5e9e100ce6c53d9ff22c8255
Phillip Ryan <phillipryan@n2-108-141.dhcp.drexel.edu>
2016-08-08 17:08:11 -0400

    Added abstract methods

--- Checkers/src/Client.java
+++ Checkers/src/Client.java
@@ -1,4 +1,27 @@
+/**
+ * Client, inherits from networking class used for Checkers game
+ * 
+ * @author phillipryan
+ */
 
 public class Client extends Network{
-
+	protected void StartNetworking(){
+		
+	}
+	
+	protected void Connect(String ipAddress){
+		
+	}
+	
+	protected void SendMove(){
+		
+	}
+	
+	protected void RecvMove(){
+		
+	}
+	
+	protected void CloseNetworking(){
+		
+	}
 }
--- Checkers/src/Network.java
+++ Checkers/src/Network.java
@@ -1,4 +1,21 @@
+/**
+ * Networking class used for Checkers game
+ * 
+ * @author phillipryan
+ */
 
-public abstract class Network {
-
+abstract class Network {
+	
+	/**
+	 * The constructor.
+	 */
+	protected Network() {
+		super();
+	}
+	
+	abstract protected void StartNetworking();
+	abstract protected void Connect(String ipAddress);
+	abstract protected void SendMove();
+	abstract protected void RecvMove();
+	abstract protected void CloseNetworking();
 }
