572955a44a42a3122953d56503b1a247b9c52b2b
Daniel Fitzick <dwf35@drexel.edu>
2016-08-08 22:29:41 -0400

    Added most of move and c_move. I still need to know exactly how these should interact with networking and gui.

--- src/Move.java
+++ src/Move.java
@@ -1,4 +1,20 @@
 
 public class Move {
-
+	private Square start;
+	private Square end;
+	
+	public void get_start_pos(){
+		//what should this function return?
+	}
+	
+	public void get_end_pos(){
+		//see above
+	}
+	
+	public void apply(){
+		end.set_piece(start.get_piece);
+		stare.remove_piece();
+		
+		//Networking has to happen. How are we doing that?
+	}
 }
