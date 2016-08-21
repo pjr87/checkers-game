de06c062301dde78d5927986479aeed8378956c8
Troy Santry <Troy@Troys-Air.home>
2016-08-21 01:45:49 -0400

    king jump

--- src/Square.java
+++ src/Square.java
@@ -125,13 +125,13 @@ public class Square extends JLabel {
 						moves.add(new C_Move(this, neighbour.neighbours.get(Direction.UpRight), neighbour));
 					}
 				}
-				else if((this.neighbours.get(Direction.DownLeft)!=null) && this.neighbours.get(Direction.DownLeft)==neighbour){
-					if(neighbour.neighbours.get(Direction.DownLeft)!=null && neighbour.neighbours.get(Direction.DownLeft).getPiece()==null && neighbour.neighbours.get(Direction.DownLeft).getPiece() instanceof King){
+				else if((this.neighbours.get(Direction.DownLeft)!=null) && this.neighbours.get(Direction.DownLeft)==neighbour && this.getPiece() instanceof King){
+					if(neighbour.neighbours.get(Direction.DownLeft)!=null && neighbour.neighbours.get(Direction.DownLeft).getPiece()==null ){
 						moves.add(new C_Move(this, neighbour.neighbours.get(Direction.DownLeft), neighbour));
 					}
 				}
-				else if((this.neighbours.get(Direction.DownRight)!=null) && this.neighbours.get(Direction.DownRight)==neighbour){
-					if(neighbour.neighbours.get(Direction.DownRight)!=null && neighbour.neighbours.get(Direction.DownRight).getPiece()==null && neighbour.neighbours.get(Direction.DownLeft).getPiece() instanceof King){
+				else if((this.neighbours.get(Direction.DownRight)!=null) && this.neighbours.get(Direction.DownRight)==neighbour && this.getPiece() instanceof King){
+					if(neighbour.neighbours.get(Direction.DownRight)!=null && neighbour.neighbours.get(Direction.DownRight).getPiece()==null){
 						moves.add(new C_Move(this, neighbour.neighbours.get(Direction.DownRight), neighbour));
 					}
 				}
