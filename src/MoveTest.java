8e814dcd929c91fa7377cf006549b52d64c9762f
Karishma Changlani <karishmachanglani@gmail.com>
2016-08-21 15:05:32 -0400

    My tests. Not that many sorry

---
+++ src/MoveTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveTest {
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet_start_pos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet_end_pos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet_captured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMake_send_string() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeMove() {
+		fail("Not yet implemented");
+	}
+
+}
